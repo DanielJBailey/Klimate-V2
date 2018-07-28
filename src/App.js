54d3eae23e88c02987e0596d77136085f91803bc
DanielJBailey <Dbailey0210@gmail.com>
2018-07-28 12:14:56 -0500

    Initialize state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      finished: false,
+      input: '',
+      location: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
-
       </div>
     );
   }
