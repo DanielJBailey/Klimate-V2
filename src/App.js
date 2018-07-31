2fff2f2613e8d998019f2e13ad0960e594c589ed
DanielJBailey <Dbailey0210@gmail.com>
2018-07-30 20:57:37 -0500

    establish weather API

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ class App extends Component {
     this.getPosition = this.getPosition.bind(this);
     this.setLocation = this.setLocation.bind(this);
     this.userDenied = this.userDenied.bind(this);
+    this.getWeather = this.getWeather.bind(this);
+  }
+
+  componentWillMount() {
+    this.getPosition();
   }
 
   //functin to handle input from zip code form (to come)
@@ -43,34 +48,50 @@ class App extends Component {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
+    //if lat & long is found, get current weather
+    if(this.state.longitude !== null || this.state.latitude !== null){
+      this.getWeather(this.state.latitude, this.state.longitude);
+    }   
   }
 
+  //function to get weather from API
+  getWeather(lat, long) {
+    let api = 'http://api.openweathermap.org/data/2.5/weather?';
+    let key = '38f67fb8c90f3e0155a438937a83b123';
+
+    fetch(api + 'lat=' + lat + '&lon=' + long + '&APPID=' + key)
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      console.log(data.weather[0].main);
+    });
+  }
+
+
+  //http://api.openweathermap.org/data/2.5/weather?lat=33.5135909&lon=-86.7791919&APPID=38f67fb8c90f3e0155a438937a83b123
   
 
   render() {
-
-    function SearchBox(props) {
-      const allowed = props.allowed;
-      if(!allowed) {
-        return (
-          <div>
+    // Search function for later use
+    // function SearchBox(props) {
+    //   const allowed = props.allowed;
+    //   if(!allowed) {
+    //     return (
+    //       <div>
             
-          </div>
-        );
-      } else {
-        return null;
-      }
-    }
+    //       </div>
+    //     );
+    //   } else {
+    //     return null;
+    //   }
+    // }
 
     return (
       <div className="App">
-      <button onClick={this.getPosition}>Get Location</button>
-      
-      <SearchBox allowed = {this.state.allowed}/>
-      <div className="results">
-        <h4>{this.state.longitude}</h4>  
-        <h4>{this.state.longitude}</h4>    
-      </div>
+        <div className="results">
+          <h4>{this.state.latitude}</h4>  
+          <h4>{this.state.longitude}</h4>    
+        </div>
       </div>
     );
   }
